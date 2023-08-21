eab06d07b718f932b0bae75c6a11285039350258
chaoting-sun <chaoting0108@gmail.com>
2023-08-21 16:55:41 +0800

    push all

--- Bashscript/infer/p_sampling.sh
+++ Bashscript/infer/p_sampling.sh
@@ -5,20 +5,20 @@
 
 GPU_IDX=0
 BENCHMARK=moses
-MODEL=pvaetf1
+MODEL=pvaetf4
 EPOCH=15
 
-CUDA_VISIBLE_DEVICES=${GPU_IDX} CUDA_LAUNCH_BLOCKING=1 python -u   \
-    inference.py                                                   \
+CUDA_VISIBLE_DEVICES=${GPU_IDX} CUDA_LAUNCH_BLOCKING=1 nohup python -u \
+    inference.py \
         -use_cond2lat \
-    p-sampling                                         \
+    p-sampling \
         -property_list logP tPSA QED \
-        -decode_algo multinomial                                   \
+        -decode_algo multinomial \
         -data_folder /fileserver-gamma/chaoting/ML/dataset/moses/ \
-        -model_type pvaetf                                \
+        -model_type pvaetf \
         -model_name model_${EPOCH}.pt               \
         -model_folder /fileserver-gamma/chaoting/ML/cvae-transformer/Experiment-Dataset/moses/${MODEL} \
         -save_folder /fileserver-gamma/chaoting/ML/cvae-transformer/Inference-Dataset/moses/p-sampling/${MODEL}-${EPOCH} \
         -n_jobs 8 \
         -n_samples 10000 \
-    # >>${MODEL}_${GPU_IDX}.out 2>&1 & \
+    >>${MODEL}_${GPU_IDX}.out 2>&1 & \
